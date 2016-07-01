40d252cf3762645ef80ba19529a0434219b0f496
Roberto Torres Jiménez <iruga_cross@hotmail.com>
2016-07-01 08:35:50 -0500

    parte para sincronizar programacion

---
+++ Assets/Scripts/HelpMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HelpMenu : MonoBehaviour {
+    MainMenu mainMenu = new MainMenu();
+
+    Button closeButton;
+    Button fogButton;
+    Button backButton;
+    Button nextButton;
+
+    public Canvas helpMenu;
+
+    Image bombaNorm;
+    Image bombaInv;
+    Image bombaMult;
+    
+	// Use this for initialization
+	void Start () {
+        helpMenu.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    /*
+    * Metodo para cerrar el menu de ayuda y reactivacion del menu principal
+  
+    public void closeButtonPress()
+    {
+        helpMenu.enabled = false;
+        helpButton.enabled = true;
+        audioButton.enabled = true;
+        playButton.enabled = true;
+        creditsButton.enabled = true;
+        fogImage.enabled = false;
+        fogButton.enabled = false;
+    }
+      */
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+    /*
+    * Botones del canvas MainMenu
+    */
+    public Button playButton;
+    public Button helpButton;
+    public Button scoreButton;
+    public Button audioButton;
+    public Button creditsButton;
+    public AudioSource music;
+    public Canvas mainCanvas;
+
+    // Use this for initialization
+    void Start () {
+        /* Inicialización del menu de ayuda y boton de ayuda
+        */
+        helpButton = helpButton.GetComponent<Button>();
+        audioButton = audioButton.GetComponent<Button>();
+        playButton = playButton.GetComponent<Button>();
+        creditsButton = creditsButton.GetComponent<Button>();
+
+        mainCanvas = mainCanvas.GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    /*
+    * Metodo para prender el el menu de ayuda en la pantalla de inicio
+    */
+    public void helpButtonPress()
+    {
+
+    }
+    /*
+    * Metodo para prender el el menu HUD en la pantalla
+    */
+    public void deactivateMainCanvas()
+    {
+        mainCanvas.enabled = false;
+    }
+    /* 
+    * Metodo para abrir el score
+    */
+    public void scorePress()
+    {
+
+    }
+    /* 
+    * Metodo para prender o apagar el audio del juego
+    */
+    public void deactivateMusic()
+    {
+        music.enabled = false;
+    }
+    /* 
+    * Metodo para prender musica
+    */
+    public void activateMusic()
+    {
+        music.enabled = true;
+    }
+    /*
+    * Metodo para mostrar los creditos del juego 
+    */
+    public void creditsPress()
+    {
+
+    }
+    
+    /*
+    * *****METODO PARA DESACTIVAR TODOS LOS BOTONES DEL CANVAS*****
+    */
+    public void deactivateButtons()
+    {
+        playButton.enabled = false;
+        helpButton.enabled = false;
+        scoreButton.enabled = false;
+        audioButton.enabled = false;
+        creditsButton.enabled = false;
+    }
+    /*
+    * *****METODO PARA ACTIVAR TODOS LOS BOTONES DEL CANVAS*****
+    */
+    public void activateButtons()
+    {
+        playButton.enabled = false;
+        helpButton.enabled = false;
+        scoreButton.enabled = false;
+        audioButton.enabled = false;
+        creditsButton.enabled = false;
+    }
+    /* 
+    * *****FUNCIONES GET DE CADA ATRIBUTO*****
+    */
+    public Button getPlayButton()
+    {
+        return playButton;
+    }
+
+    public Button getHelpButton()
+    {
+        return helpButton;
+    }
+
+    public Button getScoreButton()
+    {
+        return scoreButton;
+    }
+
+    public Button getAudioButton()
+    {
+        return audioButton;
+    }
+
+    public Button getCreditsButton()
+    {
+        return creditsButton;
+    }
+}
