a4e08315b32783de739873fbede7bcba4dae1b48
Roberto Torres Jiménez <iruga_cross@hotmail.com>
2017-03-20 17:52:23 -0600

    Añadidos comericales 100%

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,14 +2,38 @@
 using System.Collections;
 
 public class GameController : MonoBehaviour {
-    
-	// Use this for initialization
-	void Start () {
-	
+
+    public GameObject bombN;
+    public int bombNCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Vector2 spawnValues;
+
+    // Use this for initialization
+    void Start () {
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    /*
+    public void ResizeSpriteToScreen()
+    {
+        SpriteRenderer rs = GetComponent<SpriteRenderer>();
+    if(rs == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+        double width = rs.sprite.bounds.size.x;
+        double height = rs.sprite.bounds.size.y;
+
+        double worldScreenHeight = Camera.main.orthographicSize * 2.0;
+        double worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        transform.localScale = new Vector3((float)(worldScreenWidth / width),
+                                (float)(worldScreenHeight / height),
+                                1);
+    }*/
 }
